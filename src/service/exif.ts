1daefd902908d0f19daf069c08a09a3f5e2b2305
undefined <361390990@qq.com>
2025-07-23 10:57:05 +0800

    fix

--- src/service/exif.ts
+++ src/service/exif.ts
@@ -4,6 +4,7 @@ async function Exif(image: Buffer) {
   const tag = ExifReader.load(image);
   const exifList = [];
   for (const exif of Object.entries(tag)) {
+    if (exif[0] === "Thumbnail" || exif[0] === "MakerNote") continue;
     exifList.push(`${JSON.stringify(exif[0])} : ${JSON.stringify(exif[1])}`);
   }
   return exifList;
