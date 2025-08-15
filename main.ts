7ce9b9860011adb97ab356fd15e46c9dedc6aaa3
undefined <361390990@qq.com>
2025-08-15 22:44:34 +0800

    容器重启后等待其他依赖项启动

--- main.ts
+++ main.ts
@@ -2,8 +2,10 @@ import { Init as Bot } from "@miz/ai/src/core/bot";
 import { Init as Database } from "@miz/ai/src/core/db";
 import { Init as Plugin } from "@miz/ai/src/core/plugin";
 import { Init as Timer } from "@miz/ai/src/core/timer";
+import { sleep } from "bun";
 
 async function init() {
+  await sleep(10 * 1000);
   await Database();
   await Bot();
   await Plugin();
